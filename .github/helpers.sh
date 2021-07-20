73e7a3d4a96bfe1c0db7f8a9d011da0f2c37b433
Ettore Di Giacinto <edigiacinto@suse.com>
2021-07-20 16:10:21 +0200

    test - remove

--- .github/helpers.sh
+++ .github/helpers.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 cos_version() {
-    export COS_VERSION=$(yq r packages/cos/collection.yaml 'packages.[0].version')
+    SHA=$(echo $GITHUB_SHA | cut -c1-8 )
+    echo $(/usr/bin/yq r packages/cos/collection.yaml 'packages.[0].version')-g$SHA
 }
 
 create_remote_manifest() {
     MANIFEST=$1
     cp -rf $MANIFEST $MANIFEST.remote
-    yq w -i $MANIFEST.remote 'luet.repositories[0].name' 'cOS'
-    yq w -i $MANIFEST.remote 'luet.repositories[0].enable' true
-    yq w -i $MANIFEST.remote 'luet.repositories[0].priority' 90
-    yq w -i $MANIFEST.remote 'luet.repositories[0].type' 'docker'
-    yq w -i $MANIFEST.remote 'luet.repositories[0].urls[0]' $FINAL_REPO
+    /usr/bin/yq w -i $MANIFEST.remote 'luet.repositories[0].name' 'cOS'
+    /usr/bin/yq w -i $MANIFEST.remote 'luet.repositories[0].enable' true
+    /usr/bin/yq w -i $MANIFEST.remote 'luet.repositories[0].priority' 90
+    /usr/bin/yq w -i $MANIFEST.remote 'luet.repositories[0].type' 'docker'
+    /usr/bin/yq w -i $MANIFEST.remote 'luet.repositories[0].urls[0]' $FINAL_REPO
+}
+
+drop_recovery() {
+    MANIFEST=$1
+    /usr/bin/yq d -i $MANIFEST 'packages.isoimage(.==recovery/cos-img)'
 }
